84d510229e28d487fad77bca1a02c5507104170e
Nicholas <95367571+NichoZ02@users.noreply.github.com>
2022-10-19 18:58:03 +0200

    Add files via upload

--- WebServerHTTP/index.js
+++ WebServerHTTP/index.js
@@ -4,39 +4,46 @@ var url = require('url');
 var querystring = require('querystring');
 var port = 8000;
 
-const server = http.createServer((req,res)=>{
-  
+const server = http.createServer((req,res)=>
+{
   var params = querystring.parse(url.parse(req.url).query);
   var page = url.parse(req.url).pathname;
   res.writeHead(200, {"Content-Type": "text/plain"});
-  
+
   if(page == "/primi")
-    {
-      res.write(miaUtils.primi());
-    }
+  {
+    res.write(miaUtils.primi());
+
+  }
   else if(page == "/pari")
-    {
-      res.write(miaUtils.pari());
-    }
+  {
+    res.write(miaUtils.pari());
+
+  }
   else if(page == "/dispari")
-    {
-      res.write(miaUtils.dispari());
-    }
+  {
+    res.write(miaUtils.dispari());
+
+  }
   else if(page == "/saluta")
-    {
-      res.write(miaUtils.saluta(params['nick']));
-    }
+  {
+    res.write(miaUtils.saluta(params['nick']));
+
+  }
   else if(page == "/stampasufile")
-    {
-      res.write(miaUtils.stampasufile(params['string'], params['nvolte']));
-    }
+  {
+    res.write(miaUtils.stampasufile(params['string'], params['nvolte']));
+
+  }
   else
-    {
-      res.write("I'm running as a server!!!");
-    }
+  {
+    res.write("Il server è partito!");
+
+  }
   res.end();
 })
 
-server.listen(port,()=>{
-  console.log(`server is running at http://${port}`);
+server.listen(port,()=>
+{
+  console.log(`Il server è partito al http://${port}`);
 })
